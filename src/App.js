a619671f9936de98cd6b76646886feaf1c3262c5
alaakanaan <alaa.kanaan@syndigate.info>
2019-06-30 17:16:25 +0300

    add .gitignore

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <h1>Github Finder</h1>
-            </div>
+            <h1>
+                Hello
+            </h1>
         );
     }
 }
