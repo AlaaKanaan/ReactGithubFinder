184d12e378be42cdefa7af383695b854d8eab809
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-01 16:58:10 +0300

    add .gitignore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 
     render() {
         return (
-            <Navbar title="Navbar"/>
+            <Navbar/>
         );
     }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
 
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: 'fab fa-github pr-1'
+    };
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    };
+
     render() {
         return (
-            <nav className='navbar bg-primary'>
+            <nav className='navbar bg-dark'>
                 <div className="brand text-white">
-                    <i className='fab fa-github pr-1'> </i>
+                    <i className={this.props.icon}> </i>
                     {this.props.title}
                 </div>
             </nav>
