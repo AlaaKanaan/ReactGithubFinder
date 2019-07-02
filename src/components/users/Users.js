3b593d9e334078c8ae58a5ede9257404e0578e37
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-02 13:30:23 +0300

    add .gitignore

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 const Users = ({users, loading}) => {
 
@@ -16,5 +17,11 @@ const Users = ({users, loading}) => {
         );
     }
 };
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+};
+
 
 export default Users;
