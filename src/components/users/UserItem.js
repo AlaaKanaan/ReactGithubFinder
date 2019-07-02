6dbc33153166bf67e04bdf7e370c987be8f8c299
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-02 12:00:58 +0300

    add .gitignore

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,19 +1,14 @@
 import React, {Component} from 'react';
 
 class UserItem extends Component {
-    state = {
-        id: 1,
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-    };
+
 
     render() {
-        const {login, avatar_url, html_url} = this.state;
+        const {login, avatar_url, html_url} = this.props.user;
         return (
-            <div className="card" style={{width: '15em'}}>
+            <div className="card m-2" style={{width: '12em'}}>
                 <img className="card-img-top" src={avatar_url} alt={login}/>
-                <div className="card-body">
+                <div className="card-body m-auto text-center">
                     <h5 className="card-title">{login}</h5>
                     <a href={html_url} target="_blank" rel="noopener noreferrer"
                        className="card-link btn btn-primary">Profile</a>
