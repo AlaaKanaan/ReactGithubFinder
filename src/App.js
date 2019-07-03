91384a0c701a91059a0291f910937e1a116eb5d2
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-03 16:58:54 +0300

    add .gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, Fragment} from 'react';
-import axios from 'axios';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import GithubState from './context/github/GithubState';
 
@@ -13,20 +12,8 @@ import User from './components/users/User';
 
 const App = () => {
 
-    const [repos, setRepos] = useState([]);
-    const [loading, setLoading] = useState(false);
     const [alert, setAlert] = useState(null);
 
-
-    // get a users repos github
-    const getUserRepos = async (username) => {
-        setLoading(true);
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-        setRepos(res.data);
-        setLoading(false);
-    };
 
 
     // set alert
@@ -53,14 +40,8 @@ const App = () => {
                                 </Fragment>
                             )}/>
                             <Route exact path='/about' component={About}/>
-                            <Route exact path='/user/:login' render={props => (
-                                <User {...props}
-                                      getUserRepos={getUserRepos}
-                                      repos={repos}
-                                />
-                            )}/>
+                            <Route exact path='/user/:login' compmnent={User}/>
                         </Switch>
-
                     </div>
                 </div>
             </Router>
