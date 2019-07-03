a8830498d10dcde124a55d81e77e43ce6a4547b1
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-03 11:43:38 +0300

    add .gitignore

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom'
 
 const Navbar = ({icon, title}) => {
     return (
-        <nav className="navbar navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <a className="navbar-brand" href='/'>
                 <i className={icon}> </i>
                 <span className='text-react-color small'>
                         {title}
                 </span>
             </a>
+
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/" className='nav-link text-white'>Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className='nav-link text-white'>About</Link>
+                </li>
+            </ul>
         </nav>
     );
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from 'react';
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>
+                About Page
+            </h1>
+            <p>Github Search</p>
+        </Fragment>
+    );
+
+};
+
+export default About;
