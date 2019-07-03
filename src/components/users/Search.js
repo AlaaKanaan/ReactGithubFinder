b1dd898be918d8e8feee2398dbe0cf4e3c55b690
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-03 14:28:25 +0300

    add .gitignore

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,55 +1,52 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import PropTypes from "prop-types";
 
-class Search extends Component {
+const Search = ({showClear, clearUsers, searchUsers, setAlert}) => {
 
-    state = {
-        text: ''
-    };
+    const [text, setText] = useState('');
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    };
 
-    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+    const onChange = (e) => setText(e.target.value);
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.text === '') {
-            this.props.setAlert('Please enter search text', 'danger');
+        if (text === '') {
+            setAlert('Please enter search text', 'danger');
         } else {
-            this.props.searchUsers(this.state.text);
+            searchUsers(text);
         }
     };
 
-    render() {
 
-        const {showClear, clearUsers} = this.props;
-        return (
-            <div className="search-form">
-                <form onSubmit={this.onSubmit} className="form border-0">
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" value={this.state.text}
-                               onChange={this.onChange}
-                               name="text"
-                               placeholder="Search users..."
-                               aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-                        <div className="input-group-append">
-                            <button className="btn btn-primary" type="submit">Search</button>
-                            {showClear && (
-                                <button className="btn btn-danger" onClick={clearUsers}
-                                        type="button">Clear</button>
-                            )}
+    return (
+        <div className="search-form">
+            <form onSubmit={onSubmit} className="form border-0">
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" value={text}
+                           onChange={onChange}
+                           name="text"
+                           placeholder="Search users..."
+                           aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" type="submit">Search</button>
+                        {showClear && (
+                            <button className="btn btn-danger" onClick={clearUsers}
+                                    type="button">Clear</button>
+                        )}
 
-                        </div>
                     </div>
-                </form>
-            </div>
-        );
-    }
-}
+                </div>
+            </form>
+        </div>
+    );
+};
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+};
+
 
 export default Search;
